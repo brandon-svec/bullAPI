de9f4d7553ac3d324b8644e9598d3089d39d8e11
Brandon Svec <bsvec@salesforce.com>
2021-12-14 09:44:49 -0800

    allow server to listen

--- config/production.js
+++ config/production.js
@@ -1,6 +1,9 @@
 require('events').EventEmitter.prototype._maxListeners = 100;
 
 const config = {
+  http: {
+    server: null
+  },
   redis: {
     connection: process.env.REDIS_URL,
     options: {
